77f93284ca67ec1d14a3c598b35de4594105a2c7
Serkhani <dak.boniface@gmail.com>
2022-12-15 12:46:51 +0000

    removed doorpainte

--- lib/app/modules/home/views/_view.dart
+++ lib/app/modules/home/views/_view.dart
@@ -64,22 +64,6 @@ class HouseView extends GetView {
   }
 }
 
-class DoorClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path leftporchdoor = Path();
-    leftporchdoor.moveTo(size.width * 0.1875000, size.height * 0.3060000);
-    leftporchdoor.lineTo(size.width * 0.2025000, size.height * 0.3080000);
-    leftporchdoor.lineTo(size.width * 0.2000000, size.height * 0.3600000);
-    leftporchdoor.lineTo(size.width * 0.1875000, size.height * 0.3560000);
-    leftporchdoor.lineTo(size.width * 0.1875000, size.height * 0.3060000);
-    leftporchdoor.close();
-    return leftporchdoor;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
 
 class DoorsPainter extends CustomPainter {
   @override
