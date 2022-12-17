e857995083b17c98f9f024e0ed0e917581b4fa79
Serkhani <dak.boniface@gmail.com>
2022-12-17 09:40:05 +0000

    added dialogs for onTap

---
+++ lib/app/modules/homeui/widgets/door_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DoorDialog extends StatelessWidget {
+  const DoorDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
