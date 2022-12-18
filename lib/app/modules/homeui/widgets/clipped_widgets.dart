71d125f8f518fefc8c5931870c3008d12b69be5d
Serkhani <dak.boniface@gmail.com>
2022-12-18 22:05:50 +0000

    hotfix: added color personalization

--- lib/app/modules/homeui/widgets/clipped_widgets.dart
+++ lib/app/modules/homeui/widgets/clipped_widgets.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class Clip extends StatelessWidget {
   final CustomClipper<Path> clipper;
   final void Function() onPressed;
-  const Clip({super.key, required this.clipper, required this.onPressed});
+  final Color color;
+  const Clip(
+      {super.key,
+      required this.clipper,
+      required this.onPressed,
+      this.color = Colors.grey});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +18,7 @@ class Clip extends StatelessWidget {
         onTap: onPressed,
         child: Container(
           constraints: const BoxConstraints.expand(height: 360.0),
-          color: Colors.teal,
+          color: color,
         ),
       ),
     );
